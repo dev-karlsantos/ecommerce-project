96651f65ce4b010afcfcb51cffdee62de9e91bae
rgeli90 <rgeli90@gmail.com>
2023-11-28 16:53:37 -0600

    Added sign up, sign in, sign out functionality and display signed in user in the navbar.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_user_name, if: :user_signed_in?
+
+  def set_user_name
+    @username = current_user.username
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: %i[username])
+  end
 end
